3eee0cf63456d4e934b0764691aad0c28cb1122d
strbikanna <strbik.ann2000@gmail.com>
2023-09-16 14:07:04 +0200

    Revert "data model added"
    This reverts commit 848c7be7de65918b4bdeaae4971aef028b3b18f4.

---
+++ backend/auth_server/src/main/kotlin/hu/bme/aut/auth_server/AuthServerApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.auth_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthServerApplication>(*args)
+}
