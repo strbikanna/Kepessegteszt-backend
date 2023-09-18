e377b771a520dd44a7ceaa81e72bd78a205d9a2c
strbikanna <strbik.ann2000@gmail.com>
2023-09-18 07:23:32 +0200

    auth server: security in progress

---
+++ backend/auth_server/src/main/kotlin/hu/bme/aut/auth_server/config/AuthServerConfig.kt
@@ -0,0 +1,129 @@
+package hu.bme.aut.auth_server.config
+
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+
+@Configuration
+@EnableWebSecurity
+class AuthServerConfig {
+
+    @Bean
+    @Order(1)
+    fun authServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+            .oidc(withDefaults())
+
+        http // Redirect to the login page when not authenticated from the
+            // authorization endpoint
+            .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+                exceptions
+                    .defaultAuthenticationEntryPointFor(
+                        LoginUrlAuthenticationEntryPoint("/login"),
+                        MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                    )
+            } // Accept access tokens for User Info and/or Client Registration
+
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize.anyRequest().authenticated()
+            }
+            // Form login handles the redirect to the login page from the authorization server filter chain
+            .formLogin(withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val userDetails: UserDetails = User.withUsername("testUser")
+            .password("{noop}password")
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(userDetails)
+    }
+
+    @Bean
+    fun registeredClientRepository(jdbcTemplate: JdbcTemplate): RegisteredClientRepository {
+        val registeredClient = RegisteredClient.withId("e4a295f7-0a5f-4cbc-bcd3-d870243d1b05")
+            .clientId("trustable-client-00112")
+            .clientSecret("{noop}123")
+            .redirectUri("https://oauth.pstmn.io/v1/callback")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .scope(OidcScopes.OPENID)
+            .build()
+
+        val registeredClientRepository = JdbcRegisteredClientRepository(jdbcTemplate)
+        registeredClientRepository.save(registeredClient)
+
+        return registeredClientRepository
+    }
+
+    @Bean
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val jwkSet = JWKSet(generateRsaKey())
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun authorizationServerSettings(): AuthorizationServerSettings? {
+        return AuthorizationServerSettings.builder().issuer("http://localhost:9000").build()
+    }
+
+    private fun generateRsaKey(): RSAKey {
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGenerator.initialize(2048)
+        val keyPair = keyPairGenerator.generateKeyPair()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        return RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+    }
+}
