4c8b479323d37ae2536d044513f3de26c98754d5
strbikanna <strbik.ann2000@gmail.com>
2023-09-25 22:02:02 +0200

    auth server: custom login page and registration

--- backend/auth_server/build.gradle.kts
+++ backend/auth_server/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation ("org.flywaydb:flyway-core")
