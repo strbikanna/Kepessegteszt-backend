460eba0ed1a770c5ffd982520e24309f31b744b1
strbikanna <strbik.ann2000@gmail.com>
2023-10-06 15:56:26 +0200

    gameplay-start: game list with mock data

---
+++ frontend/cognitive-app-frontend/src/app/games/games.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {TEXTS} from "../utils/app.text_messages";
+import {Observable, of, Subject} from "rxjs";
+import {GameplayModel} from "../model/gameplay.model";
+
+@Component({
+    selector: 'app-games',
+    templateUrl: './games.component.html',
+    styleUrls: ['./games.component.scss']
+})
+export class GamesComponent implements OnInit {
+
+    public text = TEXTS.games;
+    public gamesForYou : Observable<GameplayModel[]> = new Observable<GameplayModel[]>()
+
+    ngOnInit(): void {
+        this.gamesForYou = of(
+            [
+                {
+                    id: "balloon001-223-554",
+                    name: "Tick the balloons",
+                    description: "Try not to die from the bombs.",
+                    thumbnail: "../../assets/balloon_game.jpg",
+                    config: JSON.stringify({level: 1}),
+                },
+                {
+                    id: "radio-num-001-223-554",
+                    name: "Radio buttons",
+                    description: "Push the correct buttons of the secret radio.",
+                    thumbnail: "../../assets/number_game.jpg",
+                    config: JSON.stringify({level: 1}),
+
+                }
+            ]
+        )
+    }
+
+}
