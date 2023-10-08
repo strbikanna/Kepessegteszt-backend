b255225323517a89d7f3015c4ac7c6ab7a42b539
strbikanna <strbik.ann2000@gmail.com>
2023-10-08 16:12:32 +0200

    feature-users: UserController

--- backend/resource_server/resource_server/src/main/kotlin/hu/bme/aut/resource_server/user/UserController.kt
+++ backend/resource_server/resource_server/src/main/kotlin/hu/bme/aut/resource_server/user/UserController.kt
@@ -1,10 +1,26 @@
 package hu.bme.aut.resource_server.user
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
-class UserController(@Autowired private var userService: UserService){
+class UserController(@Autowired private var userService: UserService) {
+
+    @GetMapping("/profile")
+    @ResponseStatus(HttpStatus.OK)
+    fun getUserProfile(authentication: Authentication): UserProfileDto {
+        val username = authentication.name
+        return userService.getUserWithProfileByUsername(username)
+    }
+
+    @PutMapping("/profile")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.OK)
+    fun updateUserProfile(@RequestBody userProfileDto: UserProfileDto) {
+        userService.updateUserProfile(userProfileDto)
+    }
 }
