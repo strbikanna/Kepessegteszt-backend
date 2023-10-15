b9e622abf343ad9b16dc698cc7312bc0b38a40f7
strbikanna <strbik.ann2000@gmail.com>
2023-10-15 14:32:18 +0200

    feature/gameplay:  gameplay repo tests

--- backend/resource_server/resource_server/src/main/kotlin/hu/bme/aut/resource_server/gameplay/GameplayRepository.kt
+++ backend/resource_server/resource_server/src/main/kotlin/hu/bme/aut/resource_server/gameplay/GameplayRepository.kt
@@ -2,7 +2,9 @@ package hu.bme.aut.resource_server.gameplay
 
 import hu.bme.aut.resource_server.user.UserEntity
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDateTime
 
 interface GameplayRepository: CrudRepository<GamePlay, Long> {
     fun findAllByUser(user: UserEntity): List<GamePlay>
+    fun findAllByUserAndTimestampBetween(user: UserEntity, start: LocalDateTime, end: LocalDateTime = LocalDateTime.now()): List<GamePlay>
 }
