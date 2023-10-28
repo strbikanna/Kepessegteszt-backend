e4096c02dda7415a40944cf30dd0af524cc6baad
strbikanna <strbik.ann2000@gmail.com>
2023-10-28 16:16:37 +0200

    backend: game dto refactored to support game_id json attribute

--- backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/gameplay/GameplayDto.kt
+++ backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/gameplay/GameplayDto.kt
@@ -1,11 +1,15 @@
 package hu.bme.aut.resource_server.gameplay
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
 data class GameplayDto(
 
     val gameResult: Map<String, Any?>,
 
     val username: String,
 
+    @JsonProperty("game_id")
     val gameId: Int
 ) {
 }
