f87776a7e1b4a1260d1b77d8faec3403c7ca7a92
strbikanna <strbik.ann2000@gmail.com>
2023-11-01 15:54:13 +0100

    frontend: NG100 error solved, impersonation-component works better

--- frontend/cognitive-app-frontend/src/app/header/header.component.ts
+++ frontend/cognitive-app-frontend/src/app/header/header.component.ts
@@ -1,22 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {UserInfo} from "../auth/userInfo";
 import {Role} from "../utils/constants";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
-  loginStatus = false;
-  isAdmin = false;
+export class HeaderComponent implements OnInit {
+    loginStatus = false
+    isAdmin = false;
 
+    constructor(private changeDetectorRef: ChangeDetectorRef) {
+    }
 
-  ngOnInit(): void {
-    UserInfo.loginStatus.subscribe(loginSuccess => {
-        this.loginStatus = loginSuccess
-      this.isAdmin = UserInfo.currentUser?.roles.find(role => role.toUpperCase() === Role.ADMIN) !== undefined  && this.loginStatus
-    });
-  }
+    ngOnInit(): void {
+        UserInfo.loginStatus.subscribe(loginSuccess => {
+            this.loginStatus = loginSuccess
+            this.isAdmin = UserInfo.currentUser?.roles.find(role => role.toUpperCase() === Role.ADMIN) !== undefined && loginSuccess
+            this.changeDetectorRef.detectChanges()
+        });
+    }
 
 }
