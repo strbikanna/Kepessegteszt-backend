02bee329cf97ab88f91ef220b2d9a050a29c7461
strbikanna <strbik.ann2000@gmail.com>
2023-11-13 20:41:18 +0100

    backend: model creation, storing

---
+++ backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/recommendation/ModelManager.kt
@@ -0,0 +1,50 @@
+package hu.bme.aut.resource_server.recommendation
+
+import hu.bme.aut.resource_server.profile_calculation.error.CalculationException
+import jep.JepException
+import jep.SharedInterpreter
+import org.springframework.beans.factory.annotation.Value
+import java.io.File
+
+/**
+ * Manages the neural network models for each game. Stores and reads JSON files containing the models.
+ * Creates new models.
+ */
+object ModelManager {
+    @Value("\${app.python.models-location}")
+    private lateinit var modelDir: String
+
+    @Value("\${app.python.models-location}")
+    private lateinit var scriptDir: String
+
+    fun getEstimationForResult(gameId: Int, abilityValues: List<Double?>): Double{
+        TODO()
+    }
+
+    fun createNewModel(gameId: Int, abilityValues: List<List<Double?>>, resultValues: List<Double>){
+        try{
+            val interpreter = SharedInterpreter()
+            interpreter.use {
+                interpreter.exec("import sys")
+                interpreter.exec("sys.path.append('$scriptDir')")
+                interpreter.exec("from app import *")
+                interpreter.set("matrix", abilityValues)
+                interpreter.set("numbers", resultValues)
+                interpreter.exec("model = calcRatio(matrix, numbers)")
+                val exportedModel = interpreter.getValue("model")
+                saveModel(gameId, exportedModel as String)
+            }
+        }catch(ex: JepException){
+            throw CalculationException("Exception in creating model of game: ${ex.message}")
+        }
+    }
+    private fun loadModel(gameId: Int): String{
+        val file = File("$modelDir$gameId.json")
+        return file.readText()
+    }
+    private fun saveModel(gameId: Int, model: String){
+        val file = File("$modelDir$gameId.json")
+        file.writeText(model)
+    }
+
+}
