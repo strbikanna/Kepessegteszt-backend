a789249aad92f037b5e661ac1d6b0a1194f91eab
strbikanna <strbik.ann2000@gmail.com>
2023-11-14 16:50:12 +0100

    frontend: new favicon

--- frontend/cognitive-app-frontend/src/app/game-management/edit-game-form/edit-game-form.component.ts
+++ frontend/cognitive-app-frontend/src/app/game-management/edit-game-form/edit-game-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Ability} from "../../model/ability.model";
 
 @Component({
   selector: 'app-edit-game-form',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class EditGameFormComponent {
 
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    thumbnail: new FormControl('', Validators.required),
+    active: new FormControl(false, Validators.required),
+    abilities: new FormControl<Ability[]>([], Validators.required),
+    configDescription: new FormControl('', Validators.required),
+  })
 }
