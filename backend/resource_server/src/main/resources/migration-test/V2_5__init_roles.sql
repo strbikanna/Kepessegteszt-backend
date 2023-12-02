0428b77a247fe9c6c38c24607f6ddca45685bfd2
strbikanna <strbik.ann2000@gmail.com>
2023-12-02 10:41:45 +0100

    backend: fixed migration version issue

---
+++ backend/resource_server/src/main/resources/migration-test/V2_5__init_roles.sql
@@ -0,0 +1,13 @@
+-- roles
+insert into role
+values('ADMIN');
+insert into role
+values('STUDENT');
+insert into role
+values('SCIENTIST');
+insert into role
+values('TEACHER');
+insert into role
+values('PARENT');
+insert into role
+values('GAME');
