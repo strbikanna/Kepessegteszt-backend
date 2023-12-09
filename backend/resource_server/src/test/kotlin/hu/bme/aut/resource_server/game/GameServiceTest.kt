695b7354d87df41ed6e7d9ce90f37d74a83b2224
Matias429 <balazsmatyas429@gmail.com>
2023-12-09 16:04:10 +0100

    Remove GameDTO

--- backend/resource_server/src/test/kotlin/hu/bme/aut/resource_server/game/GameServiceTest.kt
+++ backend/resource_server/src/test/kotlin/hu/bme/aut/resource_server/game/GameServiceTest.kt
@@ -25,37 +25,6 @@ class GameServiceTest(
 
     @Transactional
     @Test
-    fun shouldSaveGameThenReturnDto() {
-        val config = mutableMapOf<String, Any>("Level" to 0)
-        val game = GameEntity(
-            version = 1, name = "TestGame", description = "test_game_description", thumbnailPath = Paths.get("src", "test", "kotlin/hu/bme/aut/resource_server", "game", "resources", "test_game_icon.png").toString(),
-                active = true, url = "test_game_url", configDescription = config, affectedAbilities = mutableSetOf(testUtilsService.abilityColorsense))
-        gameRepository.save(game)
-        assertNotNull(game.id)
-        assertEquals(gameService.getAllGames(0, 10)[0].javaClass, GameDto::class.java)
-    }
-
-    @Transactional
-    @Test
-    fun shouldSaveMultipleGamesThenReturnDtos() {
-        val config1 = mutableMapOf<String, Any>("Level" to 0)
-        val config2 = mutableMapOf<String, Any>("Level" to 3, "Ability" to "Gf")
-        val game1 = GameEntity(
-            version = 1, name = "TestGame1", description = "test_game_description1", thumbnailPath = Paths.get("src", "test", "kotlin/hu/bme/aut/resource_server", "game", "resources", "test_game_icon.png").toString(),
-            active = true, url = "test_game_url1", configDescription = config1, affectedAbilities = mutableSetOf(testUtilsService.abilityColorsense))
-        val game2 = GameEntity(
-            version = 1, name = "TestGame2", description = "test_game_description2", thumbnailPath = Paths.get("src", "test", "kotlin/hu/bme/aut/resource_server", "game", "resources", "test_game_icon.png").toString(),
-            active = true, url = "test_game_url2", configDescription = config2, affectedAbilities = mutableSetOf(testUtilsService.abilityGf, testUtilsService.abilityGq))
-        gameRepository.save(game1)
-        gameRepository.save(game2)
-        val games = gameService.getAllGames(0, 10)
-        for (game in games) {
-            assertEquals(game.javaClass, GameDto::class.java)
-        }
-    }
-
-    @Transactional
-    @Test
     fun testFindGameDtoById() {
         val config1 = mutableMapOf<String, Any>("Level" to 0)
         val config2 = mutableMapOf<String, Any>("Level" to 3, "Ability" to "Gf")
