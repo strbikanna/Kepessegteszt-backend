b440a9dc79329e12ea4a94b0dc4182bd864a785a
strbik.anna <extern.anna.viktoria.strbik@volkswagen.de>
2024-02-23 13:29:47 +0100

    datamodel: created organization, group, subscription and extended user with new fields (birthdate, address, subscription)

---
+++ backend/resource_server/src/main/resources/db/migration/V3_4__user_extended.sql
@@ -0,0 +1,8 @@
+
+alter table user add column birth_date date;
+alter table user add column subscription varchar(100);
+alter table user add constraint fk_user_subscription foreign key (subscription) references subscription(_name);
+alter table user add column address_house_number varchar(10);
+alter table user add column address_street varchar(100);
+alter table user add column address_city varchar(100);
+alter table user add column address_postal_code varchar(10);
