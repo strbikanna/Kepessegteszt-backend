51eb2e255bb3a0cf968b9c59feda37f29f25db28
strbik.anna <extern.anna.viktoria.strbik@volkswagen.de>
2024-04-18 09:10:22 +0200

    res-server: more games

--- backend/resource_server/src/main/resources/db/migration/V1_5__init_data.sql
+++ backend/resource_server/src/main/resources/db/migration/V1_5__init_data.sql
@@ -13,6 +13,20 @@ VALUES (6,'Űrfordítás', 'Segíts a robotnak a fordításban, találd meg a he
     "maxLevel": 10
   }');
 
+INSERT INTO game(id, _name, _description, thumbnail_path, _active, url, config_description)
+VALUES (7,'Űrlénykihallgatás', 'Segíts a robotnak a tolmácsolásban!',
+        'http://localhost:8090/game_images/translator-robot.jpg', true, null, '{
+    "gameName": "word-listening",
+    "gameTitle": "Űrtolmácsolás",
+    "timelimit": "Time limit for each question in milliseconds.",
+    "number_of_questions": "Number of questions in the game.",
+    "word_length": "Length of the words in the game.",
+    "change_letter_number": "Number of letters to change in the words.",
+    "probably_changed": "Probability of changing a letter in the word.",
+    "winFieldName": "gameWon",
+    "maxLevel": 10
+  }');
+
 -- recommended games
 INSERT INTO recommended_game(_timestamp, config, _level, recommendee_id, game_id)
 VALUES (now(), '{
@@ -22,3 +36,12 @@ VALUES (now(), '{
   "word_length": 6,
   "number_of_letters_to_change": 1
 }', 5, 1, 6);
+
+INSERT INTO recommended_game(_timestamp, config, _level, recommendee_id, game_id)
+VALUES (now(), '{
+  "timelimit": 10000,
+  "number_of_questions": 5,
+  "word_length": 3,
+  "change_letter_number": 1,
+  "probably_changed": 0.5
+}', 5, 1, 7);
