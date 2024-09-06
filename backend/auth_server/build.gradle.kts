1893048d3b5216bc45d50ce2de336172d3ddfc40
jgbor <100523032+jgbor@users.noreply.github.com>
2024-09-06 16:10:51 +0200

    auth server

--- backend/auth_server/build.gradle.kts
+++ backend/auth_server/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.flywaydb.flyway") version "9.22.1"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
-	kotlin("plugin.jpa") version "1.8.22"
+	alias(libs.plugins.springframework.boot)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.kotlin.jpa)
+	alias(libs.plugins.flyway)
 }
 
 group = "hu.bme.aut"
@@ -27,22 +27,22 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation ("org.flywaydb:flyway-core")
-	implementation ("org.flywaydb:flyway-mysql")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+	implementation(libs.spring.boot.starter.jpa)
+	implementation(libs.spring.boot.starter.oauth2.authorization)
+	implementation(libs.spring.boot.starter.security)
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.thymeleaf)
+    implementation(libs.spring.boot.starter.mail)
+    implementation(libs.jackson.kotlin)
+	implementation(libs.kotlin.reflect)
+	implementation (libs.flyway.core)
+	implementation (libs.flyway.mysql)
+	implementation(libs.openapi.starter.webmvc)
+	runtimeOnly(libs.mysql.connector)
+	runtimeOnly(libs.h2)
+	annotationProcessor(libs.lombok)
+	testImplementation(libs.spring.boot.starter.test)
+	testImplementation(libs.spring.security.test)
 }
 
 tasks.withType<KotlinCompile> {
