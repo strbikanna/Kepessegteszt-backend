cec7441dc5a42a836f920b729bc3a7790158491a
strbikanna <strbik.ann2000@gmail.com>
2024-09-26 11:35:17 +0200

    backend: fix sql error

--- backend/resource_server/src/main/resources/db/migration/V5_1__insert_abilities.sql
+++ backend/resource_server/src/main/resources/db/migration/V5_1__insert_abilities.sql
@@ -1,4 +1,4 @@
-insert into ability (code, name, description, type) values
+insert into ability (code, name, description, ability_type) values
 ('Gq', 'Quantitative knowledge', 'Ability to comprehend quantitative concepts and relationships and to manipulate numerical symbols.', 'FLOATING'),
 ('Grw', 'Reading and Writing ability', 'Basic reading and writing skills.', 'FLOATING'),
 ('Gs', 'Processing speed', 'Ability to perform automatic cognitive tasks, particularly when measured under pressure to maintain focused attention.', 'FLOATING'),
