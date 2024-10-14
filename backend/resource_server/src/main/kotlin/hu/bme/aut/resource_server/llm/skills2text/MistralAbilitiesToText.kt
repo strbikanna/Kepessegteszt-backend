c38773f355659fa5f7fe6837bb417e30dd4198f3
jgbor <100523032+jgbor@users.noreply.github.com>
2024-10-14 16:56:18 +0200

    refactor to use langchain

--- backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/llm/skills2text/MistralAbilitiesToText.kt
+++ backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/llm/skills2text/MistralAbilitiesToText.kt
@@ -1,27 +1,16 @@
 package hu.bme.aut.resource_server.llm.skills2text
 
-import hu.bme.aut.resource_server.llm.mistral_kmp.MistralClient
-import hu.bme.aut.resource_server.llm.mistral_kmp.domain.Message
-import hu.bme.aut.resource_server.llm.mistral_kmp.domain.ModelParams
+import dev.langchain4j.model.mistralai.MistralAiChatModel
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class MistralAbilitiesToText(
-    @Value("\${llms.mistral.api-key}") key: String,
-    @Value("\${llms.mistral.model-name}") private val model: String
-) : AbilitiesToTextService(key) {
-    private val mistral by lazy { MistralClient(apiKey = apiKey) }
-
-    override suspend fun generateFromPrompt(prompt: String): String {
-
-        val chatResult = mistral.chat(
-            model = model,
-            messages = listOf(Message(content = prompt)),
-            params = ModelParams(safePrompt = false),
-        )
-        val result = chatResult.getOrNull()?.get(0)?.content ?: "No response from Mistral."
-        log(prompt = prompt, response = result)
-        return result
-    }
+    @Value("\${llms.mistral.api-key}") apiKey: String,
+    @Value("\${llms.mistral.model-name}") modelName: String
+) : AbilitiesToTextService() {
+    override val model: MistralAiChatModel = MistralAiChatModel.builder()
+        .apiKey(apiKey)
+        .modelName(modelName)
+        .build()
 }
