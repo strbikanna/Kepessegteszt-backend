a109c7b1eaa42f5ec0b8c9e2297a6d843a7a50f1
strbikanna <strbik.ann2000@gmail.com>
2025-10-10 12:01:57 +0200

    update langchain4j

--- backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/llm/chat_service/TestAbilitiesToTextService.kt
+++ backend/resource_server/src/main/kotlin/hu/bme/aut/resource_server/llm/chat_service/TestAbilitiesToTextService.kt
@@ -1,12 +1,12 @@
 package hu.bme.aut.resource_server.llm.chat_service
 
-import dev.langchain4j.model.chat.ChatLanguageModel
+import dev.langchain4j.model.chat.ChatModel
 import hu.bme.aut.resource_server.llm.abilities2text.AbilitiesToTextService
 import org.springframework.stereotype.Service
 
 //@Profile("test", "local")
 @Service
 class TestAbilitiesToTextService(
-    override val model: ChatLanguageModel
+    override val model: ChatModel
 ): AbilitiesToTextService() {
 }
